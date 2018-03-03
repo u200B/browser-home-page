3cc3006c090c3b00136a2269bc6d372fab63ddb8
u200B <robertzautke@gmail.com>
2018-03-02 22:39:53 -0800

    IndexedDBP selectAll and DeleteAll functionality
    refactor work

--- javascript/IndexedDBManager/IndexedDBManager.js
+++ javascript/IndexedDBManager/IndexedDBManager.js
@@ -1,20 +1,11 @@
 class IndexedDBManager {
-  static _loadDatabase() {
-    this.dbName = 'ImageDatabase'
-    this.dbp = new IndexedDBP(this.dbName)
-    this.dbp.upgrade((event) => {
-      let db = event.target.result
-      let objectStore = db.createObjectStore('images', {keyPath: undefined, autoIncrement: true})
-      objectStore.createIndex('image', 'image')
-    })
-  }
-
   static storeImage(file) {
     return new Promise((resolve, reject) => {
       this._loadDatabase()
       this.dbp.open()
       .then(event => {
-        return this.dbp.transaction(this.dbp.db.transaction('images', 'readwrite')
+        return this.dbp.transaction(this.dbp.db
+          .transaction('images', 'readwrite')
           .objectStore('images').put(file))
       })
       .then(event => {
@@ -30,9 +21,12 @@ class IndexedDBManager {
 
   static getImageByKey(key) {
     return new Promise((resolve, reject) => {
+      this._loadDatabase()
       this.dbp.open()
       .then(event => {
-        return this.dbp.transaction(this.dbp.db.transaction('images', 'readonly').objectStore('images').get(key))
+        return this.dbp.transaction(this.dbp.db
+          .transaction('images', 'readonly')
+          .objectStore('images').get(key))
       })
       .then(event => {
         this.dbp.close()
@@ -42,6 +36,59 @@ class IndexedDBManager {
         console.error(error)
         reject(error)
       })
+    })
+  }
+
+  static getAllImages() {
+    this._loadDatabase()
+    return new Promise((resolve, reject) => {
+      this.dbp.open()
+      .then(event => {
+        return this.dbp.transaction(this.dbp.db
+          .transaction('images', 'readonly')
+          .objectStore('images').getAll())
+      })
+      .then(event => {
+        this.dbp.close()
+        resolve(event)
+      })
+      .catch(error => {
+        console.error(error)
+        reject(error)
+      })
+    })
+  }
+
+  static clearAllImages() {
+    this._loadDatabase()
+    return new Promise((resolve, reject) => {
+      this.dbp.open()
+      .then(event => {
+        return this.dbp.transaction(this.dbp.db
+          .transaction('images', 'readwrite')
+          .objectStore('images').clear())
+      })
+      .then(event => {
+        this.dbp.close()
+        resolve(event)
+      })
+      .catch(error => {
+        console.error(error)
+        reject(error)
+      })
+    })
+  }
+
+  static _loadDatabase() {
+    this.dbName = 'ImageDatabase'
+    this.dbp = new IndexedDBP(this.dbName)
+    this.dbp.upgrade((event) => {
+      let db = event.target.result
+      let objectStore = db.createObjectStore(
+                            'images',
+                            {keyPath: undefined, autoIncrement: true}
+                          )
+      objectStore.createIndex('image', 'image')
     })
   }
 }
--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,7 @@ class ImageHandler {
 			return IndexedDBManager.getImageByKey(event.target.result)
 		})
 		.then(event => {
-			return HTML5FileAPIManager.convertFileToDataUrl(file)
+			return HTML5FileAPIManager.convertFileToDataUrl(event.target.result)
 		})
 		.then(dataUrl => {
 			document.getElementById('body').background = dataUrl
@@ -17,4 +17,46 @@ class ImageHandler {
 			throw new Error(error)
 		})
 	}
+
+	static clearAllImages() {
+		IndexedDBManager.clearAllImages()
+	}
 }
+
+class SettingsHandler {
+	static toggleSettingsVisibility() {
+		if (this.settingsVisible) {
+			document.getElementById('settings-container').style.display = 'none'
+			document.getElementById('content-container').style.display = 'inline'
+		} else {
+			document.getElementById('content-container').style.display = 'none'
+			document.getElementById('settings-container').style.display = 'inline'
+		}
+		this.settingsVisible = !this.settingsVisible
+	}
+}
+
+class OnLoad {
+	static loadLastSetImage() {
+		IndexedDBManager.getAllImages()
+		.then(event => {
+			if (event.target.result.length) {
+				return HTML5FileAPIManager.convertFileToDataUrl(
+					event.target.result[event.target.result.length - 1]
+				)
+			} else {
+				Promise.resolve(false)
+			}
+		})
+		.then(dataUrl => {
+			if (dataUrl) {
+				document.getElementById('body').background = dataUrl
+			}
+		})
+		.catch(error => {
+			throw new Error(error)
+		})
+	}
+}
+
+OnLoad.loadLastSetImage()
