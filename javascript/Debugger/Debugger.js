d62cde008ffdf8cdaabcf0dfb8c52a29deaf4ded
robertzautke <robertzautke@gmail.com>
2018-03-03 15:09:26 -0800

    display object errors

--- javascript/Debugger/Debugger.js
+++ javascript/Debugger/Debugger.js
@@ -2,7 +2,8 @@ class Debugger {
   constructor() {
     console.basefn = console.error
     console.error = (message) => {
-        // append error to dom here
+        message = typeof message === 'object' ?
+          JSON.stringify(message, null, 2) : message
         document.getElementById('debugger')
           .innerHTML += '<p>' + message + '</p>' + '</br>'
         console.basefn(message)
